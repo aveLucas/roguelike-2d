70029e1de25e3a737b582fe75466b6d1f5d05d54
devLucas <gitavedev@gmail.com>
2024-10-06 01:04:42 -0300

    Testes com Hitbox
    --Checagens

--- Jogo sem nome/Assets/Scripts/Player/Player_Controller.cs
+++ Jogo sem nome/Assets/Scripts/Player/Player_Controller.cs
@@ -9,20 +9,29 @@ using UnityEngine.Events;
 public class Player_Controller : MonoBehaviour
 {
     //Variables
+    [Header("-Floats-")]
     public float Speed;
     public float JumpF;
     private float Hmovement;
-    private bool isJumping;
+    public float gravityScale;
+    [Header("-Bools-")] 
     private bool FacingRight;
-    private bool Crouching;
+    
+    public bool Crouching;
+    public bool isGrounded;
+    public bool onWall;
     Rigidbody2D rigidB;
     Animator anime;
     public Collider2D standingC;
+    public Collider2D crouchingC;
+    
 
     void Start()
     {
         rigidB = GetComponent<Rigidbody2D>();
         anime = GetComponent<Animator>();
+        
+        crouchingC.enabled = false;
     }
 
     
@@ -44,11 +53,16 @@ public class Player_Controller : MonoBehaviour
 
     void Jump()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded && !Crouching)
         {
             
             rigidB.AddForce(new Vector2(0, JumpF) * JumpF);
         }
+        if(Input.GetKeyDown(KeyCode.Space) && onWall){
+            transform.position += new Vector3(0, gravityScale, 0) / 2 * Time.deltaTime;
+            rigidB.AddForce(new Vector2(0, JumpF) * JumpF);
+        }
+        
     }
 
 
@@ -65,16 +79,18 @@ public class Player_Controller : MonoBehaviour
 
     void Crouch()
     {
-        if(Input.GetKeyDown(KeyCode.LeftShift) && !isJumping)
+        if(Input.GetKeyDown(KeyCode.LeftShift) && isGrounded)
         {
             Crouching = true;
             standingC.enabled = false;
+            crouchingC.enabled = true;
             anime.SetBool("isCrouching", true);
         }
         else if(Input.GetKeyUp(KeyCode.LeftShift))
         {
             Crouching = false;
             standingC.enabled = true;
+            crouchingC.enabled = false;
             anime.SetBool("isCrouching", false);
         }
     }
@@ -84,13 +100,19 @@ public class Player_Controller : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.layer == 3){
-            isJumping = false;
+            isGrounded = true;
+        }
+        if(collision.gameObject.layer == 7){
+            onWall = true;
         }
     }
     void OnCollisionExit2D(Collision2D collision)
     {
         if(collision.gameObject.layer == 3){
-            isJumping = true;
+            isGrounded = false;
+        }
+        if(collision.gameObject.layer == 7){
+            onWall = false;
         }
     }
     
